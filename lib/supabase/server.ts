30d2b57636eb1dc3bc912c3b040c47526bcc09c6
Dhanya-cloud <dhanyalokesh004@gmail.com>
2026-02-18 17:22:29 +0530

    Refactor setAll method to use TypeScript syntax

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,14 +12,18 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
+        setAll(cookiesToSet: any[]) {
+  try {
+    cookiesToSet.forEach(({ name, value, options }) =>
+      cookieStore.set(name, value, options)
+    );
+  } catch {
+    // Server component - ignore
+  }
+},
+
             // Server component - ignore
-          }
+        }
         },
       },
     }
